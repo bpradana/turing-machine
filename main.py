19cf3929614fc4cdd22f6baa408955285c366ccd
Bintang Pradana Erlangga Putra <bintangpradana02@gmail.com>
2021-05-02 20:42:29 +0700

    add error handling and running time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 
 class TuringMachine:
@@ -10,19 +11,42 @@ class TuringMachine:
         self.padding = ['_' for _ in range(padding)]
         self.tape_head = padding
 
-    def check(self, input_string):
-        input_string = self.padding + list(input_string) + self.padding
+    def check(self, input_string, padding='center'):
+        start_time = time.time()
+        if padding == 'center':
+            input_string = self.padding + list(input_string) + self.padding
+            current_tape_head = self.tape_head
+
+        if padding == 'left':
+            input_string = self.padding*2 + list(input_string)
+            current_tape_head = self.tape_head*2
+
+        if padding == 'right':
+            input_string = list(input_string) + self.padding*2
+            current_tape_head = 0
+
         current_state = self.init
-        current_tape_head = self.tape_head
         
+        original = input_string.copy()
+
         print('Current State: %s' % current_state)
         print(' '.join(input_string))
         print(' '.join(['^' if _ == current_tape_head else ' ' for _ in range(len(input_string))]))
 
         while current_state not in self.accept:
-            current_transition = self.transitions[current_state][input_string[current_tape_head]]
-            current_state = current_transition[0]
-            input_string[current_tape_head] = current_transition[1]
+            try:
+                current_transition = self.transitions[current_state][input_string[current_tape_head]]
+                current_state = current_transition[0]
+            except:
+                print('=== TRANSITION ERROR ===')
+                break
+
+            try:
+                input_string[current_tape_head] = current_transition[1]
+            except:
+                print('=== TAPE LENGTH EXCEEDED ===')
+                break
+
             if current_transition[2] == 'R':
                 current_tape_head += 1
             if current_transition[2] == 'L':
@@ -33,14 +57,19 @@ class TuringMachine:
             print('Current State: %s' % current_state)
             print(' '.join(input_string))
             print(' '.join(['^' if _ == current_tape_head else ' ' for _ in range(len(input_string))]))
+        
+        end_time = time.time()
 
         if current_state in self.accept:
-            print('ACCEPTED')
+            print('=== ACCEPTED ===')
+            print('input  : %s' % ''.join(original))
+            print('output : %s' % ''.join(input_string))
+            print('finished in %.3f seconds' % (end_time-start_time))
 
 
 if __name__ == '__main__':
     file_name = 'transition.json'
-    input_string = '00010001'
+    input_string = '00000100001'
 
     tm = TuringMachine(file_name)
-    tm.check(input_string)
+    tm.check(input_string, padding='right')
